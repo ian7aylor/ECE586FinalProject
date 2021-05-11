99f7facb0e50fce55068cbde6ad53bc2f0e5d036
ian7aylor <ian7aylor@gmail.com>
2021-05-10 23:27:18 -0700

    Added Report

---
+++ CPP/main.cpp
@@ -0,0 +1,29 @@
+/*/////////////////////////////////////////////////////
+ECE586 Final Project
+
+
+*//////////////////////////////////////////////////////
+
+
+
+
+
+#include <stdio.h>
+using namespace std;
+
+
+/*
+This is the main function. It 
+
+
+
+*/
+int main(){
+
+int variable[] = {0, 3, 5};
+
+variable[0] = variable[1] + variable[2];
+
+printf("Hello World, %d", variable[0]);
+
+}
